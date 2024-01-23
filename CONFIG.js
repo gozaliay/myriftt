c5ad115e3b9256fa9269ec30187bd9f8d599cec5
Niaga Gozali <gozaliay96@outlook.com>
2024-01-23 15:44:21 +0700

    first commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hii sayanggg!! i have something to say <3. </br>",
+  descriptionHeader:
+    "Ini bukan apa apa, cuma apa apa yang gaada apa apanya hwhe",
+  buttonYes: "Okayyy!!",
+  buttonNo: "g, mksh",
+  titleModar: "Cảm ơn cậu.",
+  descriptionModar:
+  "Thank you for giving me a chance so I can stay by your side, just to protect and protect you. </br> I know I'm not handsome, not very talented as well, but here i am. You are my whole world. </br> Makasih banyakk makasih. Thanks for giving ur smile, the cutest smile ever, thanks for giving ur love, the sweetest love ever since .</br> God, thanks for giving me a huge chance to having Rifthia, a beautiful girl with various sides of heartful and love.</br> Aku harap aku bisa terus jadi yang terbaik buat kamu, kasih yang terbaik buat kamu. Wish me luck! as I'm wishing u all the time. Love you!! <3 </br> Written with love -Gozzz : )",
+};
